f9fd9ec74b5e97a088aa34d6c0ef252d3ce73f12
tobargerardo <147001644+tobargerardo@users.noreply.github.com>
2025-11-21 14:40:14 -0600

    Remove heading from App component
    Removed the heading 'Checkout Form' from the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-// src/App.tsx
-import React from 'react';
-import MyForm from './MyForm';
+import React from "react";
+import MyForm from "./MyForm";
 
 function App() {
   return (
     <div>
-      <h1>Checkout Form</h1>
       <MyForm />
     </div>
   );
