986001a1e163791d31abdf57c813739ca8222e95
tobargerardo <147001644+tobargerardo@users.noreply.github.com>
2025-11-21 13:38:28 -0600

    Create App component with Checkout Form

---
+++ App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import React from 'react';
+import MyForm from './MyForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Checkout Form</h1>
+      <MyForm />
+    </div>
+  );
+}
+
+export default App;
